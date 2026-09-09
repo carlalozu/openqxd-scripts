40d2b5d36de3fd1153b3bc0e6db13dceda719c31
Carla Lopez <carlalopez3108@gmail.com>
2026-09-09 09:59:27 +0200

    mode cache dirs to run script

--- build_quda.sh
+++ build_quda.sh
@@ -13,23 +13,3 @@ computer=daint
 cp $SCRIPTS_DIR/CMakePresets.json $QUDA_DIR/CMakePresets.json
 cmake -S quda/ -B quda-build --preset="$computer-quda"
 cmake --build quda-build -- -j16
-
-## Run tests
-
-l0=16
-l1=16
-l2=16
-l3=16
-
-./quda-build/tests/plaq_test --tdim $l0 --xdim $l1 --ydim $l2 --zdim $l3 --prec double
-./quda-build/tests/gauge_path_test --tdim $l0 --xdim $l1 --ydim $l2 --zdim $l3 --prec double
-
-
-### TODO: caching parameters
-export CACHE_DIR=$SCRATCH/quda-build/cache
-
-# rm -rf $CACHE_DIR
-mkdir -p $CACHE_DIR
-
-export QUDA_RESOURCE_PATH=$CACHE_DIR
-export QUDA_PROFILE_OUTPUT_BASE=$CACHE_DIR
